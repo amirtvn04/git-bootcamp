fc99d2499c287984dc110e7148d6cd8095eb086b
amirtavana <amirtvn80@gmail.com>
2025-11-21 17:25:32 +0330

    feat(theme): add light mode support

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
-sidebarBtn.addEventListener("click", function() {elementToggleFunc(sidebar); })
+sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); })
 
 //Activating Modal-testimonial
 
@@ -48,10 +48,10 @@ const selectItems = document.querySelectorAll('[data-select-item]');
 const selectValue = document.querySelector('[data-select-value]');
 const filterBtn = document.querySelectorAll('[data-filter-btn]');
 
-select.addEventListener('click', function () {elementToggleFunc(this); });
+select.addEventListener('click', function () { elementToggleFunc(this); });
 
-for(let i = 0; i < selectItems.length; i++) {
-    selectItems[i].addEventListener('click', function() {
+for (let i = 0; i < selectItems.length; i++) {
+    selectItems[i].addEventListener('click', function () {
 
         let selectedValue = this.innerText.toLowerCase();
         selectValue.innerText = this.innerText;
@@ -64,8 +64,8 @@ for(let i = 0; i < selectItems.length; i++) {
 const filterItems = document.querySelectorAll('[data-filter-item]');
 
 const filterFunc = function (selectedValue) {
-    for(let i = 0; i < filterItems.length; i++) {
-        if(selectedValue == "all") {
+    for (let i = 0; i < filterItems.length; i++) {
+        if (selectedValue == "all") {
             filterItems[i].classList.add('active');
         } else if (selectedValue == filterItems[i].dataset.category) {
             filterItems[i].classList.add('active');
@@ -80,8 +80,8 @@ const filterFunc = function (selectedValue) {
 let lastClickedBtn = filterBtn[0];
 
 for (let i = 0; i < filterBtn.length; i++) {
-    
-    filterBtn[i].addEventListener('click', function() {
+
+    filterBtn[i].addEventListener('click', function () {
 
         let selectedValue = this.innerText.toLowerCase();
         selectValue.innerText = this.innerText;
@@ -100,11 +100,11 @@ const form = document.querySelector('[data-form]');
 const formInputs = document.querySelectorAll('[data-form-input]');
 const formBtn = document.querySelector('[data-form-btn]');
 
-for(let i = 0; i < formInputs.length; i++) {
+for (let i = 0; i < formInputs.length; i++) {
     formInputs[i].addEventListener('input', function () {
-        if(form.checkValidity()) {
+        if (form.checkValidity()) {
             formBtn.removeAttribute('disabled');
-        } else { 
+        } else {
             formBtn.setAttribute('disabled', '');
         }
     })
@@ -115,18 +115,40 @@ for(let i = 0; i < formInputs.length; i++) {
 const navigationLinks = document.querySelectorAll('[data-nav-link]');
 const pages = document.querySelectorAll('[data-page]');
 
-for(let i = 0; i < navigationLinks.length; i++) {
-    navigationLinks[i].addEventListener('click', function() {
-        
-        for(let i = 0; i < pages.length; i++) {
-            if(this.innerHTML.toLowerCase() == pages[i].dataset.page) {
+for (let i = 0; i < navigationLinks.length; i++) {
+    navigationLinks[i].addEventListener('click', function () {
+
+        for (let i = 0; i < pages.length; i++) {
+            if (this.innerHTML.toLowerCase() == pages[i].dataset.page) {
                 pages[i].classList.add('active');
                 navigationLinks[i].classList.add('active');
                 window.scrollTo(0, 0);
             } else {
                 pages[i].classList.remove('active');
-                navigationLinks[i]. classList.remove('active');
+                navigationLinks[i].classList.remove('active');
             }
         }
     });
 }
+
+// switch dark & light mode - save mode
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "light") {
+    document.documentElement.classList.add("light-theme");
+    document.getElementById("darkmode-toggle").checked = true;
+}
+
+document.getElementById("theme-toggle").addEventListener("change", () => {
+    const root = document.documentElement;
+
+    root.classList.toggle("light-theme");
+
+    if (root.classList.contains("light-theme")) {
+        localStorage.setItem("theme", "light");
+        document.getElementById("darkmode-toggle").checked = true;
+    } else {
+        localStorage.setItem("theme", "dark");
+        document.getElementById("darkmode-toggle").checked = false;
+    }
+});
